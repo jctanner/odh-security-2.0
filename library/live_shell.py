d331470ef5d34465c46c2f47dab28a3f6f7d1cb1
jctanner <tanner.jc@gmail.com>
2025-08-20 17:42:01 -0400

    cleanup plugin

--- library/live_shell.py
+++ library/live_shell.py
@@ -3,10 +3,11 @@
 # Copyright: (c) 2024, OpenDataHub Security Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 
-DOCUMENTATION = r'''
+DOCUMENTATION = r"""
 ---
 module: live_shell
 short_description: Execute shell commands with live output streaming
@@ -33,9 +34,9 @@ options:
         required: false
 author:
     - OpenDataHub Security Project
-'''
+"""
 
-EXAMPLES = r'''
+EXAMPLES = r"""
 - name: Run a build command with live output
   live_shell:
     cmd: make build
@@ -46,9 +47,9 @@ EXAMPLES = r'''
 - name: Simple command with live output
   live_shell:
     cmd: echo "Hello World"
-'''
+"""
 
-RETURN = r'''
+RETURN = r"""
 rc:
     description: The return code of the command
     type: int
@@ -65,23 +66,25 @@ changed:
     description: Whether the command execution changed the system
     type: bool
     returned: always
-'''
+"""
 
 from ansible.module_utils.basic import AnsibleModule
+
 
 def main():
     """Main module function - this is just a placeholder since the action plugin does the real work"""
     module = AnsibleModule(
         argument_spec=dict(
-            cmd=dict(type='str', required=True),
-            chdir=dict(type='path', required=False),
-            environment=dict(type='dict', required=False),
+            cmd=dict(type="str", required=True),
+            chdir=dict(type="path", required=False),
+            environment=dict(type="dict", required=False),
         ),
         supports_check_mode=False,
     )
-    
+
     # This module should never be called directly - the action plugin handles execution
     module.fail_json(msg="live_shell module should be handled by the action plugin")
 
-if __name__ == '__main__':
-    main() 
+
+if __name__ == "__main__":
+    main()
